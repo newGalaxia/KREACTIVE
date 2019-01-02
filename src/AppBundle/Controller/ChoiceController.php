bd94fc9db0492d4104cdaf76faf402f1d64f19ba
Stephanie <s.fournage@gmail.com>
2019-01-02 16:03:54 +0100

    add route for create user and create choice

---
+++ src/AppBundle/Controller/ChoiceController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace AppBundle\Controller;
+use AppBundle\Entity\Film;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Choice;
+use AppBundle\Services\OmdbApiService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class ChoiceController extends Controller
+{
+    private $omdbapiService;
+
+    public function __construct(OmdbApiService $omdbApiService)
+    {
+        $this->omdbapiService = $omdbApiService;
+    }
+
+    /**
+     * @Route("/choice", name="post_choice", methods={"POST"})
+     */
+    public function createChoice(Request $request)
+    {
+        $content = [];
+        if ($request->getContent()) {
+            $content = json_decode($request->getContent(), true);
+        }
+
+        if ( isset($content['user_id']) && isset($content['film_imdbId']) ) {
+            $choice = New Choice();
+
+            // check if user exist and set idUser value
+            $choice = $this->fillUser($choice, $content['user_id']);
+            if ($choice === false) {
+                return New JsonResponse("echec : cet utilisateur n'existe pas", 401);
+            }
+
+            //check if film exist in api omdb
+            $film = $this->omdbapiService->getFilm($content['film_imdbId']);
+            if ($film === false ) {
+                return New JsonResponse("echec : ce film n'existe pas", 401);
+            } else {
+                $film = $this->updateFilmList($film);
+                $choice->setFilm($film);
+            }
+            $entityManager = $this->get('doctrine.orm.entity_manager');
+            $entityManager->persist($choice);
+            $entityManager->flush();
+
+            return New JsonResponse("bravo vous avez enregistré votre choix ", 201);
+
+        } else {
+            return New JsonResponse("echec : paramètres manquants ", 401);
+        }
+    }
+
+    private function fillUser($choice, $userId)
+    {
+        if (isset($userId)) {
+            $user = $this->get('doctrine.orm.entity_manager')
+                ->getRepository('AppBundle:User')
+                ->find($userId);
+
+            if ($user !== null ) {
+                $choice->setUser($user);
+
+                return $choice;
+            } else {
+
+                return false;
+            }
+        }
+    }
+
+    private function updateFilmList($newFilm)
+    {
+        $entityManager = $this->get('doctrine.orm.entity_manager');
+
+        $film = $entityManager
+            ->getRepository('AppBundle:Film')
+            ->find($newFilm['imdbID']);
+
+        if ($film === null) {
+            $filmToSave = new Film();
+            $filmToSave->setImdbId($newFilm['imdbID']);
+            $filmToSave->setTitle($newFilm['Title']);
+            $filmToSave->setPoster($newFilm['Poster']);
+
+            $entityManager->persist($filmToSave);
+            $entityManager->flush();
+
+            return $filmToSave;
+        }
+
+        return $film;
+    }
+}
